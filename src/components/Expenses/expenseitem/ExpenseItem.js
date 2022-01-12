8bf1e22f38ded7adc1c97cd38dfed055ee793d1a
정지후 <loveadsadf1976@gmail.com>
2022-01-12 22:15:35 +0900

    map을 활용한 반복

--- src/components/Expenses/expenseitem/ExpenseItem.js
+++ src/components/Expenses/expenseitem/ExpenseItem.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "../expensedate/ExpenseDate";
 import "./ExpenseItems.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log("바껴라!!");
-  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Cange Title</button>
     </div>
   );
 };
