2fca38a01faf233f960ac12cab0c237b34f8acab
정지후 <loveadsadf1976@gmail.com>
2022-01-13 23:43:05 +0900

    ChartBar컴포넌트를 Chart에 가져오기

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,18 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  return <div className="chart"></div>;
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label} //label마다 고유의 값을 가지게 된다.
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
