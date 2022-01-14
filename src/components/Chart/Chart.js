67da1a4ecc57fcada1f8bc4e48d386f084d5f2aa
정지후 <loveadsadf1976@gmail.com>
2022-01-14 22:05:57 +0900

    Section5 - 77.마무리 및 다음 단계

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label} //label마다 고유의 값을 가지게 된다.
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
